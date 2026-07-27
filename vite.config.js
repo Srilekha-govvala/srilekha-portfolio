4a1ae6cf020f9a0c0d065aa3f766205f3a6175ed
Srilekha <srilekhagovvala441@gmail.com>
2026-07-27 16:41:51 +0530

    Configuration update

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-    plugins: [
+  plugins: [
     react(),
     tailwindcss(),
   ],
-  base: "/srilekha-portfolio/",
+  base: process.env.NODE_ENV === "production"
+    ? "/srilekha-portfolio/"
+    : "/",
 });
